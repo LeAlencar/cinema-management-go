047e621c0d63bd3b14e2ae5ffa3c05b1e96281b5
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-11-06 07:30:59 -0300

    feat(api): add first endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,11 @@ func main() {
 
 	r := gin.Default()
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
